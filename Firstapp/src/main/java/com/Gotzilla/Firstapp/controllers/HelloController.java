23bf543785554a58366583110d512ac5e071fa76
Pforce1994 <pforce72@gmail.com>
2025-01-16 21:46:11 +0700

    ..

---
+++ Firstapp/src/main/java/com/Gotzilla/Firstapp/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.Gotzilla.Firstapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        System.out.println("Hello, World!");
+        return "Hello, World!5";
+    }
+}
